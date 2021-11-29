1decd2c0148b6afd61ad7586a966877d50df14ed
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2021-11-29 14:16:18 -0500

    Create myfirstpygame.py
    w

---
+++ .github/myfirstpygame.py
@@ -0,0 +1,10 @@
+# my first pygame< Ronald Durham, 11/29/21 1:58pm, v0.1
+
+import pygame, sys 
+from pygame locals import *
+
+# star pygame 
+pygame.init()
+
+#setup our window . 1
+windowsurface = pygame.display.set_mode((500, 400),0,32)
