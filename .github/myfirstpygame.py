48452f4cc37ca54ba0c6b526d977147e590fd883
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2021-12-03 14:29:14 -0500

    Update myfirstpygame.py

--- .github/myfirstpygame.py
+++ .github/myfirstpygame.py
@@ -1,7 +1,7 @@
-# my first pygame< Ronald Durham, 11/29/21 1:58pm, v0.3
+# my first pygame< Ronald Durham, 12/03/21 1:58pm, v0.3
 
 import pygame, sys 
-from pygame locals import *
+from pygame.locals import *
 
 # start pygame 
 pygame.init()
@@ -11,37 +11,37 @@ windowsurface = pygame.display.set_mode((500, 400),0,32)
 pygame.disply.set_caption('hello, world!')
 
 # Setup colors 
-Black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-Blue = (0,0,255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
 # setup font .
 basicfont = pygame.font.sysfont(none, 48)
 
 # Setup text.
-text = basicfont.render('hello, world!', true, White =, Blue)
+text = basicfont.render('hello, world!', True, WHITE , BLUE)
 textrect = text.get_rect()
 textRect.centerx= windowsurface.get_rect().centerx
 textRect.centerx= windowsurface.get_rect().centery
 
 # fill background color.
-windowsurface.fill(white)
+windowsurface.fill(WHITE)
 
 # Draw a polygon onto the screen
-pygame.draw.polygon(windowsurface, Green, ((146,0) , (291,106), (236,277), (56,277),
+pygame.draw.polygon(windowsurface, GREEN , ((146,0) , (291,106), (236,277), (56,277),
 
 # draw lines on the screen.
-pygame.draw.line(windowsurface, red (60,60), (120,60), 4)
+pygame.draw.line(windowsurface, RED (60,60), (120,60), 4)
 
 # Draw lines on the screen 
-pygame.draw.line(windowsurface, RED, (60,60), (120,60), 4)
+pygame.draw.line(windowsurface, RED , (60,60), (120,60), 4)
 pygame.draw.line(windowsurface, WHITE (75,60), (60,75), 2)
 pygame.draw.line(windowsurface, BLUE (0,150),(150,0), 1)
 
 # Draw the circle.
-pygame.Draw.circle(windowsurface, Black, (300,50),20,0)
+pygame.Draw.circle(windowsurface, BLACK , (300,50),20,0)
 
 # Draw an ellipise
 pygame.draw.rect(windowsurface, RED, (textRect.left - 20,))
