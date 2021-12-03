3dfdb8cc3343a8ecde14e607400491e50348d23c
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2021-12-03 14:48:48 -0500

    Update myfirstpygame.py

--- .github/myfirstpygame.py
+++ .github/myfirstpygame.py
@@ -8,7 +8,7 @@ pygame.init()
 
 #setup our window . 1
 windowsurface = pygame.display.set_mode((500, 400),0,32)
-pygame.disply.set_caption('hello, world!')
+pygame.display.set_caption('hello, world!')
 
 # Setup colors 
 BLACK = (0,0,0)
@@ -18,11 +18,11 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 # setup font .
-basicfont = pygame.font.sysfont(none, 48)
+basicfont = pygame.font.SysFont(None, 48)
 
 # Setup text.
 text = basicfont.render('hello, world!', True, WHITE , BLUE)
-textrect = text.get_rect()
+textRect = text.get_rect()
 textRect.centerx= windowsurface.get_rect().centerx
 textRect.centerx= windowsurface.get_rect().centery
 
@@ -30,24 +30,24 @@ textRect.centerx= windowsurface.get_rect().centery
 windowsurface.fill(WHITE)
 
 # Draw a polygon onto the screen
-pygame.draw.polygon(windowsurface, GREEN , ((146,0) , (291,106), (236,277), (56,277),
+pygame.draw.polygon(windowsurface, GREEN , ((146,0) , (291,106), (236,277), (56,277),(0,106)))
 
 # draw lines on the screen.
-pygame.draw.line(windowsurface, RED (60,60), (120,60), 4)
+pygame.draw.line(windowsurface, RED,(60,60), (120,60), 4)
 
 # Draw lines on the screen 
 pygame.draw.line(windowsurface, RED , (60,60), (120,60), 4)
-pygame.draw.line(windowsurface, WHITE (75,60), (60,75), 2)
-pygame.draw.line(windowsurface, BLUE (0,150),(150,0), 1)
+pygame.draw.line(windowsurface, WHITE, (75,60), (60,75), 2)
+pygame.draw.line(windowsurface, BLUE, (0,150),(150,0), 1)
 
 # Draw the circle.
-pygame.Draw.circle(windowsurface, BLACK , (300,50),20,0)
+pygame.draw.circle(windowsurface, BLACK , (300,50),20,0)
 
 # Draw an ellipise
 pygame.draw.rect(windowsurface, RED, (textRect.left - 20,))
 
 # Draw the text rectangle.
-pygame
+pygame.draw.rect(windowsurface,RED, (textRect.left -20,))
 
 
 
